7fbc7948d92f85cb211dff4fc428a19ad33851eb
Andrew <carrola1@gmail.com>
2017-02-05 16:04:12 -0500

    changed colors

--- calc.py
+++ calc.py
@@ -2,27 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QTextEdit, QAction, QApplication,QPushButton,QGridLayout,QWidget,QLabel
+from PyQt5.QtWidgets import QTextEdit,QApplication,QGridLayout,QWidget
 from PyQt5.QtGui import QIcon
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.textEdit = QTextEdit()
+        self.resDisp = QTextEdit(readOnly=True)
         self.initUI()
 
     def initUI(self):
-
-        self.textEdit = QTextEdit()
-        self.resDisp = QTextEdit(readOnly=True)
+        # Style
+        self.textEdit.setStyleSheet("background-color: #1f3960; color: white; font-size: 20px")
+        self.resDisp.setStyleSheet("background-color: #8191aa; font-size: 20px")
 
         self.textEdit.textChanged.connect(self.updateDisp)
-
         grid = QGridLayout()
         self.setLayout(grid)
         grid.addWidget(self.textEdit, 0, 0)
         grid.addWidget(self.resDisp, 0, 1)
-        self.setGeometry(300, 300, 350, 250)
+        self.setGeometry(600, 600, 700, 500)
         self.setWindowTitle('Main window')
         self.show()
 
